a5819a49b6f071f09055c4596946a5d65215bdf9
adevas01 <adevas01@mail.bbk.ac.uk>
2023-10-18 23:03:43 +0100

    First Commit

---
+++ ExerciseTwo.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+public class ExerciseTwo {
+
+  public static void main(String[] args) {
+
+    double[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100}; 
+    
+    System.out.print("Before rotation: " + " " + Arrays.toString(x)); 
+    System.out.println("  "); 
+    System.out.println("  "); 
+
+    for (int i = 0; i < x.length; i++) {
+      System.out.println("x[" + i + "]: " + x[i]);
+    }
+    System.out.println("  "); 
+    System.out.println("  "); 
+
+    System.out.println(Arrays.toString(rotate(x)));
+    System.out.print("After rotation: " + " " + Arrays.toString(x)); 
+    System.out.println("  "); 
+    System.out.println("  "); 
+
+    for (int i = 0; i < x.length; i++) {
+      System.out.println("x[" + i + "]: " + x[i]);
+    }
+    System.out.println("  "); 
+    System.out.println("  "); 
+  }
+    /*
+    Method rotate
+    
+    */
+    public static double[] rotate(double[] x) {
+      if (x.length <= 1) {
+        return x;
+      }      
+      double last = x[x.length - 1];
+      for (int i = x.length - 1; i > 0; i--) {
+        x[i] = x[i - 1];
+      }
+      x[0] = last;
+      return x;
+    }
+  }
+  
+
